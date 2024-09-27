6b0186aa8ddee510a71f911394a86c7b2a75cd35
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-09-27 11:00:00 -0700

    styling

--- src/MunchCentral/RestuarantCard.js
+++ src/MunchCentral/RestuarantCard.js
@@ -11,13 +11,13 @@ const RestuarantCard = (props) => {
       <img src={restuarant.image} className="w-64 h-64 object-cover" />
       <div className="flex flex-col p-2">
         <div className="flex  justify-between ">
-          <div>{restuarant.name}</div>
+          <div className="font-bold text-xl mb-2">{restuarant.name}</div>
          
           <div className="text-red-500"> <i className="fa-solid fa-star mr-1"></i>{restuarant.rating}</div>
         </div>
 
-        <div className="text-xl color-zing-500">{restuarant.location}</div>
-        <div>{"$".repeat(restuarant.cost)}</div>
+        <div className="text-l text-neutral-500"><i className="fa-solid fa-location-dot mr-1"></i>{restuarant.location}</div>
+        <div className="text-neutral-500">{"$".repeat(restuarant.cost)}</div>
         
       </div>
     </div>
@@ -26,6 +26,6 @@ const RestuarantCard = (props) => {
 
 export default RestuarantCard;
 
-s = 'hello world'
-s1 = 'hello world'
+//s = 'hello world'
+//s1 = 'hello world'
 
